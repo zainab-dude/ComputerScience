23ff9f0603334846eac0d29fbc2b6f1f79f1d1b5
zainab-dude <84669107+zainab-dude@users.noreply.github.com>
2021-12-19 23:19:35 +0500

    task11

---
+++ task11.vb
@@ -0,0 +1,36 @@
+﻿Module Module1
+    '11.not_bad:
+    Sub Main()
+        Dim sentence, string2, string3, rightstring, leftstring As String
+        Dim bfr, after As Integer
+
+        after = 0
+        bfr = 0
+        sentence = ""
+        leftstring = ""
+        rightstring = ""
+
+
+        Console.WriteLine("Enter the string : ")
+        sentence = Console.ReadLine()
+        string2 = "not"
+        string3 = "bad"
+        bfr = InStr(sentence, string2)
+        after = InStr(sentence, string3)
+
+
+        If bfr > 0 Then
+            leftstring = Left(sentence, bfr - 1)
+
+            If after > 0 Then
+                rightstring = Mid(sentence, after + 3, Len(sentence))
+
+
+            End If
+        End If
+
+        Console.WriteLine(leftstring & "good" & rightstring)
+        Console.ReadKey()
+    End Sub
+
+End Module
