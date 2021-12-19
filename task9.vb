2e7178be4304a63173439cbc390c10a2adb837dd
zainab-dude <84669107+zainab-dude@users.noreply.github.com>
2021-12-19 22:16:09 +0500

    task9

---
+++ task9.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+    '9.MixUp:
+    Sub Main()
+        Dim string1, string2, replaced1, replaced2 As String
+        Dim length1, length2 As Integer
+
+        string1 = ""
+        string2 = ""
+        replaced1 = ""
+        replaced2 = ""
+        length1 = 0
+        length2 = 0
+
+        Console.WriteLine("Enter word")
+        string1 = Console.ReadLine
+        Console.WriteLine("Enter word")
+        string2 = Console.ReadLine
+        replaced1 = Left(string1, 2)
+        replaced2 = Left(string2, 2)
+        length1 = Len(string1)
+        string1 = Right(string1, length1 - 2)
+        length2 = Len(string2)
+        string2 = Right(string2, length2 - 2)
+
+        Console.WriteLine(replaced1 & string2 & " " & replaced2 & string1)
+
+
+
+        Console.ReadKey()
+    End Sub
+End Module
