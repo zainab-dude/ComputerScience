4b46f97585e53cb75565f931294eb7256e841499
zainab-dude <84669107+zainab-dude@users.noreply.github.com>
2021-12-19 21:31:27 +0500

    Task6

---
+++ task6.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+    '6.donuts
+
+    Sub Main()
+        'Declarations
+        Dim numDonuts As String
+        Dim donuts As Integer
+
+        'Initialise
+        numDonuts = ""
+        donuts = 0
+
+
+        'Input data
+        Console.Write("Enter number of donuts: ")
+        numDonuts = Console.ReadLine
+        donuts = Val(numDonuts)
+
+        'Process
+        If donuts > 9 Then
+            Console.WriteLine("Number of donuts are: many")
+        Else
+            Console.WriteLine("Number of donuts are:" & numDonuts)
+        End If
+
+        Console.ReadKey()
+    End Sub
+
+End Module
