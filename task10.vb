48c84f5a23fe4287d38e4495fd14a7efb26d2332
zainab-dude <84669107+zainab-dude@users.noreply.github.com>
2021-12-19 22:32:25 +0500

    task10

---
+++ task10.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+    '10.verbing:
+    Sub Main()
+        Dim string1, string2 As String
+        Dim length1 As Integer
+
+        string1 = ""
+        
+        length1 = 0
+        string2 = ""
+
+        Console.WriteLine("Enter word")
+        string1 = Console.ReadLine
+        length1 = Len(string1)
+        string1 = LCase(string1)
+        string2 = Right(string1, 3)
+        If length1 <= 2 Then
+            Console.WriteLine(string1)
+        Else
+            If string2 <> "ing" Then
+                Console.WriteLine(string1 & "ing")
+            Else
+                Console.WriteLine(string1 & "ly")
+            End If
+        End If
+
+
+
+
+        Console.ReadKey()
+    End Sub
+End Module
